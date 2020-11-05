f3cba232522cede84285dc7c5c99797157ee5d8c
Mattèo Gauthier <matteo.gauthier@gmail.com>
2020-11-05 12:56:01 +0100

    Adding MainScene.cs and creating the menu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void PlayGame () {
+        // SceneManagement.
+    }
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+        
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+}
