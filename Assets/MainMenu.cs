1cfc417eb48b086a259769942740c3870a8f0462
Mattèo Gauthier <matteo.gauthier@gmail.com>
2020-11-06 09:55:09 +0100

    Moving the title menu to another scene, adding vscode plugin

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -6,18 +6,19 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
 
-    public void PlayGame () {
-        // SceneManagement.
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
     // // Start is called before the first frame update
     // void Start()
     // {
-        
+
     // }
 
     // // Update is called once per frame
     // void Update()
     // {
-        
+
     // }
 }
