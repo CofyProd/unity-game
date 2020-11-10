6c352651ad9d0c1ee9ed416971237ce4de73ce6a
Mattèo Gauthier <matteo.gauthier@gmail.com>
2020-11-10 21:01:29 +0100

    + Credits screen + Options screen + Main volume slider

---
+++ Assets/SetVolume.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SetVolume : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioMixer mixer;
+    public void setLevel(float sliderValue)
+    {
+        mixer.SetFloat("MainVol",Mathf.Log10(sliderValue) * 20);
+    }
+}
